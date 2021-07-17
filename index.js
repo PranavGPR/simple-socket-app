5639a714edb3064c242743388d3df0ca44d459c8
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-07-17 20:27:40 +0530

    Basic setup created

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.use("/", (_req, res) => {
+  res.send("Welcome to server!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}`);
+});
